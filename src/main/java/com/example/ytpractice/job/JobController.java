17ec51fc9c6ab12f921ed3c9f51f61f76d2e5a6e
AbQaadir <qaadir.inbox@gmail.com>
2024-02-19 15:48:35 +0530

    Success: multiple endpoints post, get

--- src/main/java/com/example/ytpractice/job/JobController.java
+++ src/main/java/com/example/ytpractice/job/JobController.java
@@ -1,6 +1,8 @@
 package com.example.ytpractice.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +12,28 @@ import java.util.List;
 public class JobController {
     private List<Job> jobs = new ArrayList<>();
 
+    // get all jobs
     @GetMapping("/jobs")
     public List<Job> findAll() {
         return jobs;
+    }
+
+    // get job by id
+    @GetMapping("/jobs/{id}")
+    public Job findById(long id) {
+        for (Job job : jobs) {
+            if (job.getId() == id) {
+                return job;
+            }
+        }
+        return null;
+    }
+
+
+    // create a new job post mapping
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return job + " has been added successfully";
     }
 }
